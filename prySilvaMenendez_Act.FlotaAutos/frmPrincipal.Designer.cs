49fe65414fc92690fc31d4e326abe07fa2456f30
JoacoSM18 <joako.sm.18@gmail.com>
2026-04-20 17:07:19 -0300

    Cree la rama

--- prySilvaMenendez_Act.FlotaAutos/frmPrincipal.Designer.cs
+++ prySilvaMenendez_Act.FlotaAutos/frmPrincipal.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Principal";
             this.ResumeLayout(false);
 
